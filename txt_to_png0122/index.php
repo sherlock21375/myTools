e761c119db79fafdf2d9f9eacd9336b555b5583e
sherlock21375 <sherlock21375@gmail.com>
2020-01-22 22:09:59 +0800

    删除index.php中多余部分

--- txt_to_png0122/index.php
+++ txt_to_png0122/index.php
@@ -1,34 +1,5 @@
 <?php
 header("Content-type: text/html; charset=utf-8"); 
-// bgcolor:#FDC800
-// ftcolor:#9C0002
-// $text = "wangjun";
-// $img=imagecreate(680,300);//创建一个长为500高为16的空白图片
-// imagecolorallocate($img,253,200,0xff);//设置图片背景颜色
-// $font = "./font/hanyi.ttf";
-// $fontSize = 15;//磅值字体  
-// $fontColor = imagecolorallocate($img, 0, 0, 0 );//字的RGB颜色  
-// imagettftext($img,$fontSize,0,0,16,$fontColor,$font,$text);
-// imagepng($img);//输出图片，输出png使用imagepng方法，输出gif使用imagegif方法
-
-
-// $text="wangjun";//显示的文字
-// $width = 300;
-// $size=12;//字体大小
-// $font="./font/hanyi.ttf";//字体类型，这里为黑体，具体请在windows/fonts文件夹中，找相应的font文件
-// $img=imagecreate(680,$width);//创建一个长为500高为16的空白图片
-// imagecolorallocate($img,253,200,0);//设置图片背景颜色，这里背景颜色为#ffffff，也就是白色
-// $black=imagecolorallocate($img,156,0,2);//设置字体颜色，这里为#000000，也就是黑色
-// $a = imagettfbbox($size, 0, $font, $text);   //得到字符串虚拟方框四个点的坐标
-// $len = $a[2] - $a[0];
-// $x = ($width-$len)/2;
-// $y = $a[3]-$a[5];
-// imagettftext($img,$size,0,$x,$y,$black,$font,$text);//将ttf文字写到图片中
-
-// header('Content-Type: image/png');//发送头信息
-
-// imagepng($img);//输出图片，输出png使用imagepng方法，输出gif使用imagegif方法
-
 $width  = 680;
 $height = 300;
 $size1 = 50;
